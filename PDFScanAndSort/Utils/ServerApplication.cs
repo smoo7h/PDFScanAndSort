7a9e6070fe9e9c65928e934aec16c784d8f45386
Matt <mmccann@ssolutions.ca>
2016-01-19 14:06:56 -0500

    Added Devexpress ServerAuthentication
    Added Devexpress ServerAuthentication for communication to a XPO
    database

---
+++ PDFScanAndSort/Utils/ServerApplication.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Xpo;
+
+namespace PDFScanAndSort.Utils
+{
+    public class ServerApplication : XafApplication
+    {
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args)
+        {
+            args.ObjectSpaceProvider = new XPObjectSpaceProvider(args.ConnectionString, args.Connection);
+        }
+        protected override DevExpress.ExpressApp.Layout.LayoutManager CreateLayoutManagerCore(bool simple)
+        {
+            throw new NotImplementedException();
+        }
+        public void Logon()
+        {
+            base.Logon(null);
+        }
+        protected override void CheckCompatibilityCore()
+        {
+            //base.CheckCompatibilityCore();
+        }
+
+        protected override void OnDatabaseVersionMismatch(DatabaseVersionMismatchEventArgs args)
+        {
+            args.Handled = true;
+            base.OnDatabaseVersionMismatch(args);
+        }
+
+
+    }
+}
---
+++ PDFScanAndSort/Utils/ServerAuthentication.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Security;
+
+namespace PDFScanAndSort.Utils
+{
+    public class ServerAuthentication : AuthenticationBase
+    {
+        private CriteriaOperator workflowWorkerUserCriteria;
+
+        public ServerAuthentication(CriteriaOperator workflowWorkerUserCriteria)
+        {
+            this.workflowWorkerUserCriteria = workflowWorkerUserCriteria;
+        }
+
+        public override object Authenticate(IObjectSpace objectSpace)
+        {
+            object user = objectSpace.FindObject(UserType, workflowWorkerUserCriteria);
+            if (user == null)
+            {
+                throw new AuthenticationException("", "Cannot find workflow worker user.");
+            }
+            return user;
+        }
+
+        public override Type UserType { get; set; }
+
+        public override bool AskLogonParametersViaUI
+        {
+            get { return false; }
+        }
+
+        public override bool IsLogoffEnabled
+        {
+            get { return false; }
+        }
+    }
+}
